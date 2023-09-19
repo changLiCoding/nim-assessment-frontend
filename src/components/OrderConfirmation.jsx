6c5aa861834c90ec62a162828e99ab7de5fc0a6a
javascriptsucks <evix1988@live.cn>
2023-09-19 00:33:10 -0600

    completed styles and props passing in ConfirmationPage and OrderConfirmation

--- src/components/OrderConfirmation.jsx
+++ src/components/OrderConfirmation.jsx
@@ -1,8 +1,35 @@
-export default function OrderConfirmation() {
+import styles from "./styles/ConfirmationPage.module.css";
+
+export default function OrderConfirmation({ order }) {
+  const { id, status, items, name, address } = order;
+
   return (
-    <div>
+    <div className={styles.orderInfo}>
       <h3>Thank you for your order</h3>
-      Order Confirmation
+      <div>
+        <h4>Order #{id}</h4>
+        <p>Status: {status}</p>
+        <p>Name: {name}</p>
+        <p>Address: {address}</p>
+        <p>Items:</p>
+        <ul>
+          {items.map((item) => (
+            <li key={item.item.id}>
+              {item.item.name} x {item.quantity}
+            </li>
+          ))}
+        </ul>
+        <div className={styles.subTotal}>
+          <span>Order Total: </span>
+          <span>
+            $
+            {items.reduce(
+              (acc, item) => acc + item.item.price * item.quantity,
+              0
+            )}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
